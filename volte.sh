549b22486bdee5a3430d3e4409e18c8507d2e22a
Myron <34206615+IamJoker03@users.noreply.github.com>
2018-01-04 20:39:32 +0530

    Changed branch of Telephony
    Previously was using the wrong branch of packages_services_Telephony . Updated it to the new branch.

--- volte.sh
+++ volte.sh
@@ -5,7 +5,7 @@ git clone https://github.com/omnirom/android_frameworks_opt_net_ims -b android-8
 rm -rf packages/apps/PhoneCommon
 git clone https://github.com/omnirom/android_packages_apps_PhoneCommon -b android-8.1-ims packages/apps/PhoneCommon
 rm -rf packages/services/Telephony
-git clone https://github.com/sayan7848/packages_services_Telephony.git -b 8.1-ims packages/services/Telephony
+git clone https://github.com/sayan7848/packages_services_Telephony.git -b o8.1 packages/services/Telephony
 rm -rf packages/services/Telecomm
 git clone https://github.com/omnirom/android_packages_services_Telecomm -b android-8.1-ims packages/services/Telecomm
 rm -rf frameworks/base
