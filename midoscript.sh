45cd0570d14f406238184ac02359d4d861eabeaf
Myron <34206615+IamJoker03@users.noreply.github.com>
2018-01-10 02:27:37 +0530

    Update midoscript.sh

--- midoscript.sh
+++ midoscript.sh
@@ -11,17 +11,18 @@ git clone https://github.com/Adesh15/hardware_qcom_display -b lineage-15.0-caf-8
 git clone https://github.com/Adesh15/hardware_qcom_audio -b lineage-15.0-caf-8996 hardware/qcom/audio-caf/msm8996
 git clone https://github.com/Adesh15/hardware_qcom_media.git -b lineage-15.0-caf-8996 hardware/qcom/media-caf/msm8996 
 
-#VoLTE Fix for AICP 
+#VoLTE Fix 
 rm -rf frameworks/opt/telephony
 rm -rf frameworks/opt/net/ims
 rm -rf packages/apps/PhoneCommon
 rm -rf packages/services/Telecomm
 rm -rf packages/services/Telephony
-rm -rf frameworks/base
+rm -rf vendor/qcom/opensource/dataservice
 git clone https://github.com/omnirom/android_frameworks_opt_telephony -b android-8.1-ims frameworks/opt/telephony
 git clone https://github.com/omnirom/android_frameworks_opt_net_ims -b android-8.1-ims frameworks/opt/net/ims
 git clone https://github.com/omnirom/android_packages_apps_PhoneCommon -b android-8.1-ims packages/apps/PhoneCommon
 git clone https://github.com/sayan7848/packages_services_Telephony.git -b o8.1 packages/services/Telephony
 git clone https://github.com/omnirom/android_packages_services_Telecomm -b android-8.1-ims packages/services/Telecomm
-git clone https://github.com/sayan7848/frameworks_base.git -b o8.1 frameworks/base
+git clone https://github.com/omnirom/vendor_qcom_opensource_dataservices.git -b android-8.1 vendor/qcom/opensource/dataservice
 
+#Cherry-Pick VoLTE commits! 
