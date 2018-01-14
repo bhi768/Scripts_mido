ed12d9c11c95352822eb8b362e5b5380a45cb6e7
Myron <34206615+IamJoker03@users.noreply.github.com>
2018-01-14 13:01:49 +0530

    Update midoscript.sh

--- midoscript.sh
+++ midoscript.sh
@@ -7,9 +7,9 @@ git clone https://github.com/TheScarastic/proprietary_vendor_xiaomi -b lineage-1
 rm -rf hardware/qcom/media-caf 
 rm -rf hardware/qcom/display-caf 
 rm -rf hardware/qcom/audio-caf 
-git clone https://github.com/Adesh15/hardware_qcom_display -b lineage-15.0-caf-8996 hardware/qcom/display-caf/msm8937 
-git clone https://github.com/Adesh15/hardware_qcom_audio -b lineage-15.0-caf-8996 hardware/qcom/audio-caf/msm8937
-git clone https://github.com/Adesh15/hardware_qcom_media.git -b lineage-15.0-caf-8996 hardware/qcom/media-caf/msm8937
+git clone https://github.com/Adesh15/hardware_qcom_display -b lineage-15.0-caf-8996 hardware/qcom/display-caf/msm8996 
+git clone https://github.com/Adesh15/hardware_qcom_audio -b lineage-15.0-caf-8996 hardware/qcom/audio-caf/msm8996
+git clone https://github.com/Adesh15/hardware_qcom_media.git -b lineage-15.0-caf-8996 hardware/qcom/media-caf/msm8996
 
 #VoLTE Fix 
 rm -rf frameworks/opt/telephony
