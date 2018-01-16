4a1c3ce197b5d66c8d49352004d5292d3633dbd1
Myron <34206615+IamJoker03@users.noreply.github.com>
2018-01-16 10:18:58 +0530

    Update midoscript.sh

--- midoscript.sh
+++ midoscript.sh
@@ -21,7 +21,7 @@ rm -rf vendor/qcom/opensource/dataservices
 git clone https://github.com/omnirom/android_frameworks_opt_telephony -b android-8.1-ims frameworks/opt/telephony
 git clone https://github.com/omnirom/android_frameworks_opt_net_ims -b android-8.1-ims frameworks/opt/net/ims
 git clone https://github.com/omnirom/android_packages_apps_PhoneCommon -b android-8.1-ims packages/apps/PhoneCommon
-git clone https://github.com/sayan7848/packages_services_Telephony.git -b o8.1 packages/services/Telephony
+git clone https://github.com/omnirom/android_packages_services_Telephony.git -b android-8.1-ims packages/services/Telephony
 git clone https://github.com/omnirom/android_packages_services_Telecomm -b android-8.1-ims packages/services/Telecomm
 git clone https://github.com/omnirom/vendor_qcom_opensource_dataservices.git -b android-8.1 vendor/qcom/opensource/dataservices
 
