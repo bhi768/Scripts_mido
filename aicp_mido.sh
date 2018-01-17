89e1cc5f990889d5c6eecb781030df814f6dc924
Myron <34206615+IamJoker03@users.noreply.github.com>
2018-01-18 03:10:43 +0530

    Update aicp_mido.sh

--- aicp_mido.sh
+++ aicp_mido.sh
@@ -1,6 +1,6 @@
 #Device specific BLOBS
 git clone https://github.com/sayan7848/device_xiaomi_mido.git -b bliss-8.1 device/xiaomi/mido
-git clone https://github.com/IamJoker03/android_kernel_xiaomi_msm8953.git -b o8.1 kernel/xiaomi/msm8953 
+git clone https://github.com/Adesh15/android_kernel_xiaomi_msm8953.git -b featherO_rebase kernel/xiaomi/msm8953 
 git clone https://github.com/sayan7848/proprietary_vendor_xiaomi.git -b oreo-mr1 vendor/xiaomi 
 
 #VoLTE Shit 
