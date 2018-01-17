a04908bd63ac16d4b6ae9f8d21b55336a77be3c3
Myron <34206615+IamJoker03@users.noreply.github.com>
2018-01-17 20:26:57 +0530

    Update aicp_mido.sh

--- aicp_mido.sh
+++ aicp_mido.sh
@@ -16,3 +16,15 @@ rm -rf packages/services/Telephony
 git clone https://github.com/RiteshSaxena/packages_services_Telephony/ -b 8.1-ims packages/services/Telephony
 rm -rf packages/services/Telecomm
 git clone https://github.com/omnirom/android_packages_services_Telecomm -b android-8.1-ims packages/services/Telecomm 
+
+#Gerrit 
+cd frameworks/base
+git fetch https://github.com/sayan7848/frameworks_base o8.1 
+git cherry-pick f51906c6563a2a1bb4b9806219c5af0892b9d7e1^..d137285279aaea3ca994307564fb848f9f538e60
+
+cd packages/services/Telephony 
+git fetch c2d445d62c1c00420f07036bbfa7188596f4e89f^..54607ac0dad708f6647a13315d6fc15f423c6ff7 
+
+cd frameworks/base 
+git fetch https://github.com/IamJoker03/frameworks_base ims 
+git cherry-pick cf01bc3d76a1cf7888a78065bc2dda12ebf59d4e^..63c99490c5e162a5c991e3c7854f9c1199af7e88 
