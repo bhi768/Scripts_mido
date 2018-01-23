d7189109455f6bf5cf6852306867b204096c24e8
Myron <34206615+IamJoker03@users.noreply.github.com>
2018-01-23 19:45:19 +0530

    Update aicp_mido.sh

--- aicp_mido.sh
+++ aicp_mido.sh
@@ -21,9 +21,9 @@ git clone https://github.com/omnirom/android_packages_services_Telecomm -b andro
 rm -rf hardware/qcom/power
 
 HAL's
-rm -rf hardware/qcom/display-caf 
-git clone https://github.com/Adesh15/hardware_qcom_display -b lineage-15.0-caf-8996 hardware/qcom/display-caf/msm8937 
+rm -rf hardware/qcom/display-caf
+git clone https://github.com/Adesh15/hardware_qcom_display -b lineage-15.0-caf-8996 hardware/qcom/display-caf/msm8996 
 rm -rf hardware/qcom/audio-caf 
-git clone https://github.com/Adesh15/hardware_qcom_audio -b lineage-15.0-caf-8996 hardware/qcom/audio-caf/msm8937
+git clone https://github.com/Adesh15/hardware_qcom_audio -b lineage-15.0-caf-8996 hardware/qcom/audio-caf/msm8996
 rm -rf hardware/qcom/media-caf 
-git clone https://github.com/Adesh15/hardware_qcom_media.git -b lineage-15.0-caf-8996 hardware/qcom/media-caf/msm8937 
+git clone https://github.com/Adesh15/hardware_qcom_media.git -b lineage-15.0-caf-8996 hardware/qcom/media-caf/msm8996
