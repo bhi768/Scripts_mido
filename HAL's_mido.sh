2d15dee9ac6f1d906c7ae5287f020ccee809a476
Bhimanand <bhimanand7@gmail.com>
2018-04-27 22:04:04 +0530

    Update HAL's_mido.sh

--- HAL's_mido.sh
+++ HAL's_mido.sh
@@ -2,8 +2,9 @@
 rm -rf hardware/qcom/audio-caf/msm8996
 rm -rf hardware/qcom/media-caf/msm8996
 rm -rf hardware/qcom/display-caf/msm8996
-git clone https://github.com/Adesh15/hardware_qcom_audio -b lineage-15.0-caf-8996 hardware/qcom/audio-caf/msm8996
-git clone https://github.com/Adesh15/hardware_qcom_media -b lineage-15.0-caf-8996 hardware/qcom/media-caf/msm8996
-git clone https://github.com/bhi768/hardware_qcom_display -b lineage-15.0-caf-8996 hardware/qcom/display-caf/msm8996
+git clone https://github.com/bhi768/hardware_qcom_audio -b c8.1 hardware/qcom/audio-caf/msm8996
+git clone https://github.com/bhi768/hardware_qcom_media -b c8.1 hardware/qcom/media-caf/msm8996
+git clone https://github.com/bhi768/hardware_qcom_display -b c8.1 hardware/qcom/display-caf/msm8996
+
 
 
