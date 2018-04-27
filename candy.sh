14f92df06738ccc6054e19533a194d6a55468808
Bhimanand <bhimanand7@gmail.com>
2018-04-27 22:08:49 +0530

    Create candy.sh

---
+++ candy.sh
@@ -0,0 +1,5 @@
+#device Stuff
+git clone https://github.com/bhi768/xiaomi_mido_treble -b c8.1 device/xiaomi/mido
+git clone https://github.com/bhi768/device_xiaomi_msm8953-common -b lineage-15.1 device/xiaomi/msm8953-common
+git clone https://github.com/bhi768/kernel_xiaomi_msm8953 -b c8.1-treble kernel/xiaomi/msm8953
+git clone https://github.com/bhi768/proprietary_vendor_xiaomi -b treble vendor/xiaomi
