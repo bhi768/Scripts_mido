6516ac59346c216ca678843cc45231a5740c4ff4
Bhimanand <bhimanand7@gmail.com>
2018-07-26 13:21:30 +0530

    Update candy.sh

--- candy.sh
+++ candy.sh
@@ -1,5 +1,5 @@
 #device Stuff
-git clone https://github.com/bhi768/xiaomi_mido_treble -b c8.1 device/xiaomi/mido
-git clone https://github.com/bhi768/device_xiaomi_msm8953-common -b lineage-15.1 device/xiaomi/msm8953-common
-git clone https://github.com/bhi768/kernel_xiaomi_msm8953 -b c8.1-treble kernel/xiaomi/msm8953
-git clone https://github.com/bhi768/proprietary_vendor_xiaomi -b treble vendor/xiaomi
+git clone https://github.com/CandyDevices/device_xiaomi_mido -b c8.1 device/xiaomi/mido
+git clone https://github.com/CandyDevices/device_xiaomi_msm8953-common -b c8.1 device/xiaomi/msm8953-common
+git clone https://github.com/CandyDevices/kernel_xiaomi_msm8953 -b c8.1 kernel/xiaomi/msm8953
+git clone https://github.com/TheScarastic/proprietary_vendor_xiaomi -b lineage-15.1 vendor/xiaomi
