7ca79c4fc03ca2ca87736ee240fcbef832d93d65
Bhimanand <bhimanand7@gmail.com>
2019-02-24 10:04:32 +0530

    Update script

--- clone_mido.sh
+++ clone_mido.sh
@@ -5,15 +5,17 @@ echo Removing existing stuff.
 rm -rf device/xiaomi kernel/xiaomi vendor/xiaomi
 
 echo Cloning Kernel
-git clone https://github.com/bhi768/kernel_xiaomi_msm8953/ -b c9.0-r34 kernel/xiaomi/msm8953
+git clone https://github.com/CandyDevices/kernel_xiaomi_msm8953 -b c9.0 kernel/xiaomi/msm8953
+#git clone https://github.com/bhi768/kernel_xiaomi_msm8953/ -b c9.0-r34 kernel/xiaomi/msm8953
 
 echo Cloning Vendor
-git clone https://github.com/bhi768/proprietary_vendor_xiaomi/ -b c9.0 vendor/xiaomi
+git clone https://github.com/SecretIngredients/proprietary_vendor_xiaomi -b c9.0 vendor/xiaomi
+#git clone https://github.com/bhi768/proprietary_vendor_xiaomi/ -b c9.0 vendor/xiaomi
 
 echo Cloning Device Tree
 #echo Enter branch
 #read fbranch
-git clone https://github.com/candydevices/device_xiaomi_mido/ -b c9.0 device/xiaomi/mido && git clone https://github.com/CandyDevices/device_xiaomi_msm8953-common/ -b c9.0 device/xiaomi/msm8953-common
+git clone https://github.com/candydevices/device_xiaomi_mido/ -b c9.0 device/xiaomi/mido
 
 #End Cloning
 
