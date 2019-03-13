939f6a17c93a6a8b8ac229c119f39a40afb0ba01
Bhimanand <bhimanand7@gmail.com>
2019-03-13 07:43:46 +0530

    Update hals_whyred.sh

--- hals_whyred.sh
+++ hals_whyred.sh
@@ -10,6 +10,7 @@ echo Cloning
 
 #git clone https://github.com/LiquidRemix/android_hardware_qcom_media/ -b liquid-pie-caf-8998 hardware/qcom/media-caf/msm8998 && git clone https://github.com/LiquidRemix/android_hardware_qcom_display/ -b liquid-pie-caf-8998 hardware/qcom/display-caf/msm8998 && git clone https://github.com/LiquidRemix/android_hardware_qcom_audio/ -b liquid-pie-caf-8998 hardware/qcom/audio-caf/msm8998
 git clone https://github.com/PixelExperience/hardware_qcom_media-caf_msm8998 -b pie-caf hardware/qcom/media-caf/msm8998 && git clone https://github.com/PixelExperience/hardware_qcom_display-caf_msm8998 -b pie-caf hardware/qcom/display-caf/msm8998 && git clone https://github.com/PixelExperience/hardware_qcom_audio-caf_msm8998 -b pie-caf hardware/qcom/audio-caf/msm8998
+
 echo Completed
 
 # End
